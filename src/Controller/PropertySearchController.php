65e02dcb2e7a6a668370c21bbf9ca8ef52eaba6c
Sean Smith <sean@nibbr.co.uk>
2020-12-08 02:59:21 +0000

    fix

--- src/Controller/PropertySearchController.php
+++ src/Controller/PropertySearchController.php
@@ -67,7 +67,7 @@ class PropertySearchController extends AbstractController
                     'class' => 'form-control'
                 ],
 
-                'data' => $submittedFilters['bathsMax'] == null ?: $submittedFilters
+                'data' => isset($submittedFilters['bathsMax']) ? $submittedFilters['bathsMax'] : null
 
             ])
 
